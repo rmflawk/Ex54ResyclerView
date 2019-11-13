11e0253a3aed787cef0b5cfcc356ea787ab4659f
rmflawk <ant921111@naver.com>
2019-11-13 13:46:27 +0900

    첫번째 커밋

---
+++ app/src/main/java/com/youngstudio/ex54resyclerview/MainActivity.java
@@ -0,0 +1,64 @@
+package com.youngstudio.ex54resyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    ArrayList<String> datas = new ArrayList<>();
+
+    MyAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //대량의 데이터
+        datas.add("aaa");
+        datas.add("bbb");
+        datas.add("ccc");
+        datas.add("ddd");
+        datas.add("eee");
+        datas.add("fff");
+        datas.add("ggg");
+        datas.add("aaa");
+        datas.add("bbb");
+        datas.add("ccc");
+        datas.add("ddd");
+        datas.add("eee");
+        datas.add("fff");
+        datas.add("ggg");
+
+        recyclerView= findViewById(R.id.recyclerview);
+        adapter= new MyAdapter(datas,this);
+        recyclerView.setAdapter(adapter);
+
+        //GridLaytoutManager 적용해 보기
+//        GridLayoutManager layoutManager= new GridLayoutManager(this,2);
+//        recyclerView.setLayoutManager(layoutManager);
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
